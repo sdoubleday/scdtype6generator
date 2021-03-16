7ce14ee859af6a4114a1e1afa44c612eff48c10c
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-03-15 23:02:47 -0400

    test Column.Script() . Close #7

---
+++ NUnitTests/ColumnTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SCDType6Generator.Tests
+{
+    [TestFixture]
+    public class ColumnTests
+    {
+        [TestCase]
+        public void Script()
+        {
+            //Arrange
+            String ColumnName = "ColumnName";
+            int OrdinalPosition = 1;
+            IDataType dataType = new FakeDataType();
+            IColumnDefault columnDefault = new FakeColumnDefault();
+            Nullability nullability = new Nullability("NO");
+            String Expected = "ColumnName FakeDataType NOT NULL FakeColumnDefault";
+            //Act
+            Column column = new Column(ColumnName,OrdinalPosition,columnDefault,nullability,dataType);
+            String Actual = column.Script();
+
+            //Assert
+            Assert.AreEqual(Expected, Actual);
+        }
+        
+    }
+}
