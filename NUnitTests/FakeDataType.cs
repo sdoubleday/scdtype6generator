f11597c2ab999660d8daa0fd27b6bf072fdf5592
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-03-15 22:55:43 -0400

    create FakeDataType. close #19

---
+++ NUnitTests/FakeDataType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCDType6Generator.Tests
+{
+    public class FakeDataType : IDataType
+    {
+        public string DataTypeName { get; set; }
+
+        public IDataType Clone()
+        {
+            IDataType returnable = new FakeDataType();
+            return returnable;
+        }
+
+        public void ParameterValidation()
+        {
+            return;
+        }
+
+        public string Script()
+        {
+            String returnable = "FakeDataType";
+            return returnable;
+        }
+    }
+}
