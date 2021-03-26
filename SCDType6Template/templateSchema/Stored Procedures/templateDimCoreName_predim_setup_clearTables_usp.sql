610c20c18b5238ad3d66cfc68a6c2e8aa6a955c5
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-03-25 23:54:23 -0400

    sproc to clear tables. close #34

---
+++ SCDType6Template/templateSchema/Stored Procedures/templateDimCoreName_predim_setup_clearTables_usp.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [templateSchema].[templateDimCoreName_predim_setup_clearTables_usp]
+AS
+DELETE [templateSchema].[templateDimCoreName_dimSrc_stg];
+DELETE [templateSchema].[templateDimCoreName_predim_prep];
+DELETE [templateSchema].[templateDimCoreName_predim_copycurrent];
+
