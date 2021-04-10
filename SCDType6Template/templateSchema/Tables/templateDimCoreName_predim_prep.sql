9336f8b67114054860ed88e422e6df8cda25735c
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-04-09 20:50:10 -0400

    began adding replacement points

--- SCDType6Template/templateSchema/Tables/templateDimCoreName_predim_prep.sql
+++ SCDType6Template/templateSchema/Tables/templateDimCoreName_predim_prep.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [templateSchema].[templateDimCoreName_predim_prep]
 (
-	 [NK_SourceSystemID1] INT NOT NULL
-	,[NK_SourceSystemID2] INT NOT NULL
-	,[SampleColumnOne] VARCHAR(500) NOT NULL
-	,[SampleColumnTwo] VARCHAR(500) NOT NULL
+	 [NK_SourceSystemID1] INT NOT NULL				/*NaturalKey_ReplacementPoint|NK_SourceSystemID1*/
+	,[NK_SourceSystemID2] INT NOT NULL				/*Sample*/
+	,[SampleColumnOne] VARCHAR(500) NOT NULL		/*DimensionAttribute_ReplacementPoint|SampleColumnOne*/
+	,[SampleColumnTwo] VARCHAR(500) NOT NULL		/*Sample*/
 	,[Ctl_EffectiveDate] DATETIME2(7) NOT NULL
 	,[SCD_Status] VARCHAR(30) NOT NULL
 )
