f56a670bc20bee42086793b305bf581ec8c2e72d
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-05-02 11:30:08 -0400

    skip the cleartable sproc for staging if staging is not a user defined table. close #77

---
+++ SCDType6Template/templateSchema/Stored Procedures/templateDimCoreName_Predim_ControlFlow_IsStgATable_usp.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [templateSchema].[templateDimCoreName_Predim_ControlFlow_IsStgATable_usp]
+AS
+DECLARE @returnable BIT
+IF EXISTS (
+	SELECT
+	1 AS one 
+	FROM sys.objects o
+	WHERE OBJECT_SCHEMA_NAME(o.object_id) =  'StagingSchema'
+	AND o.name like 'templateDimCoreName_dimSrc_stg'
+	AND o.type = 'U'
+)
+BEGIN
+	SET @returnable = 1
+END
+ELSE 
+BEGIN
+	SET @returnable = 0
+END
+RETURN @returnable
